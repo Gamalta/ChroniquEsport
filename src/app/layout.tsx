c39e7603f53d113ec05166c0c55e0ecb930899ab
Elio <elio.vital@ikomia.com>
2023-10-15 02:44:44 +0200

    chore: clean project

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import "@/styles/globals.css";
+import { Metadata } from "next";
+import { Providers } from "./providers";
+import clsx from "clsx";
+
+export const metadata: Metadata = {
+	//do something
+};
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<head />
+			<body
+				className={clsx(
+					"min-h-screen bg-background font-sans antialiased",
+				)}
+			>
+				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+					<div className="relative flex flex-col h-screen">
+						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+							{children}
+						</main>
+					</div>
+				</Providers>
+			</body>
+		</html>
+	);
+}
