2db35c2c78fcebe26ca8ce0987e08d32ad6feb85
Elio <elio.vital@ikomia.com>
2023-10-16 22:45:53 +0200

    feat: root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,27 @@
-import "@/styles/globals.css";
-import { Metadata } from "next";
-import { Providers } from "./providers";
-import clsx from "clsx";
+import '@/styles/globals.css';
+import {Metadata} from 'next';
+import {Providers} from './providers';
+import clsx from 'clsx';
 
 export const metadata: Metadata = {
-	//do something
+  //do something
 };
 
-export default function RootLayout({
-	children,
-}: {
-	children: React.ReactNode;
-}) {
-	return (
-		<html lang="en">
-			<head />
-			<body
-				className={clsx(
-					"min-h-screen bg-background font-sans antialiased",
-				)}
-			>
-				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-					<div className="relative flex flex-col h-screen">
-						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-							{children}
-						</main>
-					</div>
-				</Providers>
-			</body>
-		</html>
-	);
+export default function RootLayout({children}: {children: React.ReactNode}) {
+  return (
+    <html lang="en">
+      <head />
+      <body
+        className={clsx('min-h-screen bg-background font-sans antialiased')}
+      >
+        <Providers themeProps={{attribute: 'class', defaultTheme: 'dark'}}>
+          <div className="relative flex flex-col h-screen">
+            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+              {children}
+            </main>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  );
 }
