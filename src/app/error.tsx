b578eaeb056312e6da3fc5b350c8392f1355dd6f
Elio <elio.vital@ikomia.com>
2023-10-16 22:45:37 +0200

    fix: change error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,19 +1,12 @@
-'use client' 
- 
-import { useEffect } from 'react'
- 
+'use client';
+
 export default function Error({
   error,
   reset,
 }: {
-  error: Error
-  reset: () => void
+  error: Error;
+  reset: () => void;
 }) {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error)
-  }, [error])
- 
   return (
     <div>
       <h2>Something went wrong!</h2>
@@ -25,6 +18,7 @@ export default function Error({
       >
         Try again
       </button>
+      {error.message}
     </div>
-  )
+  );
 }
