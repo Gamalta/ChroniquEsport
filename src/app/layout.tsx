832520f4bad99037992fb2d929b335febcad1b14
Elio <elio.vital@ikomia.com>
2023-10-18 09:06:21 +0200

    chore: app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css';
 import {Metadata} from 'next';
 import {Providers} from './providers';
 import clsx from 'clsx';
+import {NavBar} from '@/components/NavBar';
 
 export const metadata: Metadata = {
   //do something
@@ -15,10 +16,9 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
         className={clsx('min-h-screen bg-background font-sans antialiased')}
       >
         <Providers themeProps={{attribute: 'class', defaultTheme: 'dark'}}>
-          <div className="relative flex flex-col h-screen">
-            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-              {children}
-            </main>
+          <NavBar />
+          <div className="container mx-auto max-w-7xl py-16 px-6 flex-grow h-screen">
+            <main className="max-w-xl3 h-full">{children}</main>
           </div>
         </Providers>
       </body>
